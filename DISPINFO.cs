63c109462eed6f555a910862d4d292f581dadcc0
luca Faggion <luc.af@live.it>
2015-10-19 05:02:44 +0200

    First Commit
    added the first autocad command DIMINC

---
+++ DISPINFO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using AutoCad_ARX;
+
+namespace Utility_funcs
+{
+    class DISPINFO
+    {
+    }
+}
